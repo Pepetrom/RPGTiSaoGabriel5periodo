cb9206e57b52b1a42ad4fcc796ed74640feab6b0
malteusta <malteus.ta@gmail.com>
2024-11-22 20:17:57 -0300

    UIImproves

---
+++ Assets/Temp/Malta/Scripts/LoadingScene.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour
+{
+    public GameObject loadingScene;
+    public GameObject button;
+    public Slider loadingFill;
+    
+    public void LoadingSceneActive(string name)
+    {
+        StartCoroutine(LoadSceneAsync(name));
+    }
+    IEnumerator LoadSceneAsync(string name)
+    {
+        AsyncOperation o = SceneManager.LoadSceneAsync(name);
+        loadingScene.SetActive(true);
+        while (!o.isDone)
+        {
+            float loadProgress = Mathf.Clamp01(o.progress/0.2f);
+            loadingFill.value = loadProgress;
+            yield return null;
+        }
+        ShowButton();
+    }
+    void ShowButton()
+    {
+        button.SetActive(true);
+    }
+}
