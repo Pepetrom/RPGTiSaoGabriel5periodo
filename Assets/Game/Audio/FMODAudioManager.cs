e29532421b2cfb79d688c5911d80180fd71e10d7
Pedro Vieira <pedrohsvnegocios@gmail.com>
2025-05-17 17:14:09 -0300

    Mais Organizacao

---
+++ Assets/Game/Audio/FMODAudioManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class FMODAudioManager : MonoBehaviour
+{
+    public static FMODAudioManager instance;
+    [Header("Ambiente")]
+    public EventReference bonfireInteract;
+    public EventReference boxCrash;
+    public EventReference notes;
+    public EventReference lever;
+    public EventReference valve;
+    public EventReference fillingWater;
+
+    [Header("player")]
+    public EventReference[] sword;
+    public EventReference estus;
+    public EventReference dash;
+    public EventReference hoffDeath;
+
+    [Header("Porquinho")]
+    public EventReference porquinDeath;
+    public EventReference porquin;
+
+    [Header("Caranguejo")]
+    public EventReference takingDamage;
+    public EventReference crabDeath;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    public void PlayOneShot(EventReference sound, Vector3 position)
+    {
+        RuntimeManager.PlayOneShot(sound, position);
+    }
+    public void PlayOneShotAttached(EventReference sound, GameObject ob)
+    {
+        RuntimeManager.PlayOneShotAttached(sound, ob);
+    }
+    public void PlayFootsteps(string path)
+    {
+        EventInstance footsteps = RuntimeManager.CreateInstance(path);
+        footsteps.start();
+        footsteps.release();
+    }
+    public void PlaySoundAttached(string path)
+    {
+        RuntimeManager.PlayOneShotAttached(path, gameObject);
+    }
+}
