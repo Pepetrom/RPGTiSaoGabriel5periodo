71ba737932e3a0bcca93962c4dae994a500acb11
Pedro Vieira <pedrohsvnegocios@gmail.com>
2025-05-23 21:28:12 -0300

    chengelog

--- Assets/Game/Scripts/Changelog/ChangeLog.cs
+++ Assets/Game/Scripts/Changelog/ChangeLog.cs
@@ -13,20 +13,23 @@ public class ChangeLog : MonoBehaviour
     public GameObject textScroller;
     public GameObject logger;
     public TextMeshProUGUI version;
+    public RectTransform contentSize;
     void Start()
     {
         changelogData = changelog.changelogData;
         foreach( var e in changelogData.entries)
         {
-            TextMeshProUGUI teste = Instantiate(logger, textScroller.transform).GetComponent<TextMeshProUGUI>();
-            teste.transform.SetParent(textScroller.transform, false);
+            GameObject tempLogger = Instantiate(logger, textScroller.transform);
+            TextMeshProUGUI loggerText = tempLogger.GetComponentInChildren<TextMeshProUGUI>();
+            tempLogger.transform.SetParent(textScroller.transform, false);
             string authors = "";
             foreach (string author in e.authors)
             {
                 authors = authors + ", " + author;
             }
-            teste.text = $"<color=#F9FF00>{e.title}</color>: " + e.content + $"<color=#00C6B5>{authors}</color>: ";
+            loggerText.text = $"<color=#F9FF00>{e.version}</color> " + $"<color=#F9FF00>{e.title}</color>: " + e.content + $"<color=#00C6B5>{authors}</color>: ";
         }
+        contentSize.sizeDelta = new Vector2(contentSize.sizeDelta.x, changelogData.entries.Count * 100) ;
         if(changelogData.entries.Count > 0) version.text = "V " + changelogData.entries[changelogData.entries.Count-1].version + " ChangeLog(click)";
     }
 }
