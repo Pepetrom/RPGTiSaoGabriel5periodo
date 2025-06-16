df78f5d37aa394ef1b99f6df414b44f3d770e591
malteusta <malteus.ta@gmail.com>
2025-06-16 11:44:16 -0300

    Start da cutscene

---
+++ Assets/CutsceneManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneManager : MonoBehaviour
+{
+    public static CutsceneManager instance;
+    public GameObject hoff, player, canvas;
+    public Camera[] cameras;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        hoff.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            hoff.SetActive(true);
+            player.SetActive(false);
+            canvas.SetActive(false);
+            ChangeCamera(0);
+        }
+    }
+    public void ChangeCamera(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                cameras[0].gameObject.SetActive(false);
+                cameras[1].gameObject.SetActive(true);
+                break;
+            case 1:
+                cameras[1].gameObject.SetActive(false);
+                cameras[2].gameObject.SetActive(true);
+                break;
+            case 2:
+                cameras[2].gameObject.SetActive(false);
+                cameras[3].gameObject.SetActive(true);
+                break;
+        }
+    }
+
+}
---
+++ Assets/HoffCutsceneReceiverr.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoffCutsceneReceiverr : MonoBehaviour
+{
+    public void ChangeCamera(int index)
+    {
+        CutsceneManager.instance.ChangeCamera(index);
+    }
+}
