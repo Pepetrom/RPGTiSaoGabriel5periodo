73777497488bd2c7439fe5f463b29a205469decb
Pedro Vieira <pedrohsvnegocios@gmail.com>
2025-06-17 00:34:38 -0300

    ElevadorAjustado + Ajustado fora do mapa visivel na tartaruga + removido rio

--- Assets/Game/Scripts/Interactable/Elevator.cs
+++ Assets/Game/Scripts/Interactable/Elevator.cs
@@ -7,6 +7,11 @@ public class Elevator : Interactable
     public ObjectThatMove objectThatMove;
     public int locationID = 0;
     bool lastActivated = false;
+    public Animator alavanca;
+    private void Start()
+    {
+        objectThatMove.ChangeLocation(0);
+    }
     public override void Interact()
     {
         Activate();
@@ -16,5 +21,6 @@ public class Elevator : Interactable
         FMODAudioManager.instance.PlayOneShot(FMODAudioManager.instance.lever, transform.position);
         objectThatMove.ChangeLocation(lastActivated ? 0 : 1);
         lastActivated = !lastActivated;
+        alavanca.SetTrigger("play");
     }
 }
